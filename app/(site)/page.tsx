2666cf2d69023907097301e70ec0dd8c1c5b0ee2
Tanish Basu <tanishbasu50@gmail.com>
2023-07-17 13:51:07 +0530

    created the nessesary frontend fro dev and created database with supabase and sql

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,29 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
+
 export default function Home() {
   return (
-    <div className='text-green-500'>Spotify</div>
+    <div className=' bg-neutral-900 h-full w-full overflow-hidden overflow-y-auto '>
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">
+            Welcome Back
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3 mt-4">
+            <ListItem image="/images/liked.png" name="Liked Songs" href="liked"/>
+          </div>
+        </div>
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justfy-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest Songs
+          </h1>
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
+    </div>
   )
 }
