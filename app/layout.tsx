b3e507873211ed85268bd125772ccd8012e3af5b
Tanish Basu <tanishbasu50@gmail.com>
2023-07-21 17:37:16 +0530

    worked on authentication and hooks and also navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -19,10 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider/>
         <SupabaseProvider>
-        <Sidebar>
-          {children}
-        </Sidebar>
+          <UserProvider>
+            <ModalProvider/>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider> 
         </SupabaseProvider>
         </body>
     </html>
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,31 @@ import {RxCaretLeft,RxCaretRight} from "react-icons/rx";
 import {HiHome} from 'react-icons/hi'
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 interface HeaderProps{
     children:React.ReactNode;
     className?:string;
 }
 const Header:React.FC<HeaderProps>= ({children,className}) => {
-  const router=useRouter()
-  const handleLogout=()=>{
-
-  }
+  const authModal=useAuthModal();
+  const router=useRouter();
+  const supabaseClient =useSupabaseClient();
+  const {user}=useUser();
+  const handleLogout=async()=>{
+    const {error} = await supabaseClient.auth.signOut();
+    //Reset any palaying song
+    router.refresh();
+    if (error) {
+        toast.error(error.message);
+    }
+    else{
+        toast.success('Logged Out!');
+    }
+}
     return (
    
     <div className={twMerge(` h-fit bg-gradient-to-b from-emerald-800 p-6`,className)}>
@@ -36,18 +52,29 @@ const Header:React.FC<HeaderProps>= ({children,className}) => {
                 </button>
             </div>
             <div className="flex justify-between items-center gap-x-4">
-                <>
-                    <div>
-                        <Button onClick={()=>{}} className="bg-transparent text-neutral-300 font-medium">
-                            Sign Up
-                        </Button>
-                    </div>
-                    <div>
-                        <Button onClick={()=>{}} className="bg-white px-6 py-2">
-                            Log In
-                        </Button>
-                    </div>
-                </>
+                {user?(
+                <div className="flex gap-x-4 items-center">
+                    <Button onClick={handleLogout} className="bg-white">
+                        Logout
+                    </Button>
+                    <Button onClick={()=>router.push('/account')} className="bg-white">
+                        <FaUserAlt/>
+                    </Button>
+                </div>
+                ):(
+                    <>
+                        <div>
+                            <Button onClick={authModal.onOpen} className="bg-transparent text-neutral-300 font-medium">
+                                Sign Up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">
+                                Log In
+                            </Button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
         {children}
